7ef39399eb1bfbc8edc06d7cc38e199518c47643
GravataLonga <jonathan.alexey16@gmail.com>
2020-03-14 14:39:17 +0000

    FIX: Change Feature Folder and ADD Bootstrap 4

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function i_can_access_to_homepage()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee(config('app.name'));
+    }
+}
